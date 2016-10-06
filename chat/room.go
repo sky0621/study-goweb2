8b36116f39419a8e3fcd36a78e8f74f4f9862e18
S.S <ss@ss.com>
2016-10-06 15:57:24 +0900

    chat

--- chat/room.go
+++ chat/room.go
@@ -1,10 +1,27 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 type room struct {
 	forward chan []byte      // 他の全てのクライアントに転送するためのメッセージを保持するチャネル
 	join    chan *client     // チャットルームに参加しようとしているクライアントのためのチャネル
 	leave   chan *client     // チャットルームから退室しようとしているクライアントのためのチャネル
 	clients map[*client]bool // 在室中の全てのクライアントを保持
+}
+
+// 構造体「チャットルーム」の初期化用メソッド
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
 }
 
 // バックグラウンドでゴルーチン実行させる用
@@ -27,4 +44,38 @@ func (r *room) run() {
 			}
 		}
 	}
+}
+
+// room の参照(*room)を http.Handler 型に適合させる。（※同じシグネチャを持つ ServeHTTP メソッドを追加するだけ）
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+// WebSocketを使うには、HTTP接続をアップグレードする必要がある
+var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
+
+// HTTPリクエストが来るたびに呼ばれるメソッド
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil) // HTTP接続をアップグレードしてソケット生成
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+
+	// 構造体「クライアント」を初期化
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize), // 指定の型のチャネルを指定バッファサイズで生成
+		room:   r,
+	}
+
+	r.join <- client // 生成したクライアントをチャットルームの入室用チャネル（join）に投入！
+
+	defer func() { r.leave <- client }() // クライアントの終了時に退室の処理を行う。ユーザがいなくなった際のクリーンナップ。
+
+	go client.write() // ゴルーチン実行
+
+	client.read() // メインスレッド上で実行。接続が保持されたまま、終了指示が出るまで他の処理をブロックする。
 }
