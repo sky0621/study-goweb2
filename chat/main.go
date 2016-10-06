d4fed2e4c1b8c0b2594462f83df08da95d57af9a
S.S <ss@ss.com>
2016-10-06 16:23:36 +0900

    flag

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -21,17 +22,21 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		func() {
 			t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 		})
-	t.templ.Execute(w, nil) // XXX 本当は戻り値をチェックすべき
+	// HTTPリクエスト情報を渡したことでHTML上でリクエスト情報が参照可能になる
+	t.templ.Execute(w, r) // XXX 本当は戻り値をチェックすべき
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() // コマンドラインで指定した -addr=":9999" 文字列から必要な情報を取得して *addr にセット
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 
 	go r.run() // チャットルーム開始 -> 入退室やメッセージを待ち受ける
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Webサーバーを開始します。ポート：", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
