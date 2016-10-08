a87fb5cacdbf18cec078be05e0c9f7b95d458c45
S.S <ss@ss.com>
2016-10-09 08:06:37 +0900

    debug

--- chat/upload.go
+++ chat/upload.go
@@ -3,12 +3,14 @@ package main
 import (
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"path/filepath"
 )
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	userID := r.FormValue("userid")
+	log.Println("r.FormFile(avatarFile)")
 	file, header, err := r.FormFile("avatarFile")
 	if err != nil {
 		io.WriteString(w, err.Error())
@@ -16,12 +18,14 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
+	log.Println("ioutil.ReadAll(file)")
 	data, err := ioutil.ReadAll(file)
 	if err != nil {
 		io.WriteString(w, err.Error())
 		return
 	}
 
+	log.Println("filepath.Join(avatarts, userID+filepath.Ext(header.Filename))")
 	filename := filepath.Join("avatarts", userID+filepath.Ext(header.Filename))
 	err = ioutil.WriteFile(filename, data, 0777)
 	if err != nil {
