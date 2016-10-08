e560cc0de2c9e1efd39a8555e268083fc33d7ef9
S.S <ss@ss.com>
2016-10-09 04:49:56 +0900

    avatar

--- chat/message.go
+++ chat/message.go
@@ -4,7 +4,8 @@ import "time"
 
 // 1メッセージ
 type message struct {
-	Name    string
-	Message string
-	When    time.Time
+	Name      string
+	Message   string
+	When      time.Time
+	AvatarURL string
 }
